519753e8545ed8d8bbd041911f22fb735aa9bd1f
Ryan Boll <bollr38@hotmail.com>
2024-04-21 15:21:24 -0500

    added debug

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config():
     FLASK_APP = os.getenv('FLASK_APP')
     FLASK_ENV = os.getenv('FLASK_ENV')
     SECRET_KEY = os.environ.get('THE_SECRET_KEY') or 'Maia Loves Cardano'
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir,'app.db')
     SQLALCHEMY_TRACK_NOTIFICATIONS = False
